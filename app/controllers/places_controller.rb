d07aae06ffcc9fdfa87111ef177c6c40d44d56ff
kahashem <kahashem@outlook.com>
2016-06-03 08:22:33 +0000

    cleaned my log in page

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+ before_action :authenticate_user!, only: [:new, :create]
+
+	def index
+		@places = Place.all
+	end
+	def index
+		@places = Place.paginate(:page => params[:page], :per_page=> 10)
+	end
+	def new
+		@place = Place.new
+	end
+	def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomster::Application.config.secret_key_base = 'd5f4f91d3507c99ea3ecfba0a08be5d2c110385e9cec6c3b197e86fea40e2e920324d428d0292b9bb5edd36975a04fd9984e0e76be36088135f17a3f05e72fe2'
